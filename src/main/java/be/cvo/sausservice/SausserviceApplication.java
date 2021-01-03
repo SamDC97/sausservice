1dd62bb5dfb7473895831ef7099a41876e00652d
SamDC97 <sam.decock97@gmail.com>
2021-01-03 11:52:48 +0100

    Initial commit

---
+++ src/main/java/be/cvo/sausservice/SausserviceApplication.java
@@ -0,0 +1,13 @@
+package be.cvo.sausservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SausserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SausserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/cvo/sausservice/SausserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cvo.sausservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SausserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
